adbd6440b399dbb8658bc9ea343e0c6ed8d5bbe6
arskdj <68353557+arskdj@users.noreply.github.com>
2020-08-13 02:48:48 +0300

    3.3

--- index.js
+++ index.js
@@ -35,6 +35,15 @@ app.get('/api/persons', (req, res) => {
     res.send(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(p => p.id == id)
+    if (person)
+        res.json(person)
+    else
+        res.status(404).end()
+})
+
 app.get('/info', (req, res) => {
     const msg = `<p> phonebook has ${persons.length} people </p> ${ Date() } `
     res.send(msg)
