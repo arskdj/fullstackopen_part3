1d15508c32c465b743b0deef9501c788fd8aef80
arskdj <68353557+arskdj@users.noreply.github.com>
2020-08-13 16:58:45 +0300

    deploy

---
+++ frontend/build/precache-manifest.cb4a49f3d066f25b1bfcebd74fa81f8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd5730fad49fe184ecd9e38f1058399",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce463561446fe03b5c4d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dbaf8825220e57e56d18",
+    "url": "/static/js/2.ad435a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad435a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce463561446fe03b5c4d",
+    "url": "/static/js/main.73b288b0.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
