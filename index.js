498f47e184a8e8fd0f199e689637fe660ebbe718
arskdj <68353557+arskdj@users.noreply.github.com>
2020-08-13 04:06:30 +0300

    3.8

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('data', req => {
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :response-time ms - :data'))
 
 const PORT = 3001
 app.listen(PORT, () => {
